89d7c502e1996d2a654c4b7823fa69f1f85fe740
Aral Sarrafi <33207285+Aral-Sarrafi@users.noreply.github.com>
2018-09-18 14:18:38 -0400

    updateWeights function implemented.

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -152,6 +152,101 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
 	//   and the following is a good resource for the actual equation to implement (look at equation 
 	//   3.33
 	//   http://planning.cs.uiuc.edu/node99.html
+
+	// Loop over all the particles to update the weights.
+
+	for (unsigned int i = 0; i < particles.size(); i++) {
+		
+		Particle p;
+		p = particles[i];
+
+		double x_p, y_p, theta_p;
+		x_p = p.x;
+		y_p = p.y;
+		theta_p = p.theta;
+
+		// For a spesific particle loop over all the landmarks to find which landmarks are in the range of sensor.
+		// populate the predicted vector with the landmarks that are in the range of sensor.
+		vector<LandmarkObs> predicted;
+		double x_lm, y_lm;
+		int ID_lm;
+		for (unsigned int j = 0; j < map_landmarks.landmark_list.size(); j++) {
+			
+			x_lm = map_landmarks.landmark_list[j].x_f;
+			y_lm = map_landmarks.landmark_list[j].y_f;
+			ID_lm = map_landmarks.landmark_list[j].id_i;
+
+			double distance_to_particle;
+			distance_to_particle = dist(x_p, y_p, x_lm, y_lm);
+
+			if (distance_to_particle <= sensor_range)
+			{
+				predicted.push_back(LandmarkObs{ x_lm, y_lm, ID_lm });
+			}
+		}
+
+		// Loop over all the observations and transform them to the particle coordinate system.
+		vector<LandmarkObs> Transformed_Obs;
+		double x_obs, y_obs;
+		int ID_obs;
+
+		double x_tObs, y_tObs;
+		for (unsigned int j = 0; j < observations.size(); j++) {
+			
+			LandmarkObs obs;
+			obs = observations[j];
+			x_obs = obs.x;
+			y_obs = obs.y;
+			ID_obs = obs.id;
+
+			x_tObs = x_p + cos(theta_p) * x_obs - sin(theta_p) * y_obs;
+			y_tObs = y_p + sin(theta_p) * x_obs + cos(theta_p) * y_obs;
+
+			Transformed_Obs.push_back(LandmarkObs{ x_tObs, y_tObs, ID_obs });
+		}
+		// execute the data association to update the IDs for the observations.
+		dataAssociation(predicted, Transformed_Obs);
+
+		// Update the weight the selected particle.
+		double alpha = 1.0;
+		// Landmark stds
+		double sigma_x, sigma_y;
+		sigma_x = std_landmark[0];
+		sigma_y = std_landmark[1];
+		// Loop over all the Transformed_Obs to computer the likelihood of each observation, then assuming that
+		// the observations are independent combine the likelihoods by mutiplying them to obtain the final weight.
+		for (unsigned int j = 0; j < Transformed_Obs.size(); j++) {
+			LandmarkObs obs;
+			obs = Transformed_Obs[j];
+
+			x_obs = obs.x;
+			y_obs = obs.y;
+			ID_obs = obs.id;
+
+			// Loop over the predicted landmarks and computer the likelihood fo the observation for the correct ID.
+			for (unsigned int k = 0; k < predicted.size(); k++) {
+				
+				if (ID_obs == predicted[k].id) {
+					double x_pr;
+					double y_pr;
+
+					x_pr = predicted[k].x;
+					y_pr = predicted[k].y;
+
+					double likelihood;
+					likelihood = (1.0 / (2 * M_PI * sigma_x * sigma_y)) *
+								exp(-(pow(x_obs - x_pr ,2)+pow(y_obs - y_pr ,2)));
+					// Assuming that the observations are independent combine them into a sinle likelihood function
+					// by multiplying them.
+					alpha *= likelihood;
+				}
+			}
+
+		}
+		// Update the weights
+		weights[i] = alpha;
+		p.weight = alpha;
+	}
 }
 
 void ParticleFilter::resample() {
